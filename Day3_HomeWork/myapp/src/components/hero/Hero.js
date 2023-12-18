6660641579d0d52fad553f379b24ccf560212a63
Jaiten <Jaitensahu9@gmail.com>
2023-12-18 20:11:50 +0530

    slider

--- Day3_HomeWork/myapp/src/components/hero/Hero.js
+++ Day3_HomeWork/myapp/src/components/hero/Hero.js
@@ -1,9 +1,23 @@
-import './Hero.css'
-export default function Hero(){
-    return (
-        <>  
-        <div className="imgContainer">
+import "./Hero.css";
+export default function Hero() {
+  return (
+    <>
+      <div className="imgContainer">
+        <div className="images">
+          <img
+            src="https://d3nn873nee648n.cloudfront.net/1200x1800-new/19164/SM902256.jpg"
+            alt=""
+          />
+          <img
+            src="https://d3nn873nee648n.cloudfront.net/1200x1800-new/19364/SM921885.jpg"
+            alt=""
+          />
+          <img
+            src="https://d3nn873nee648n.cloudfront.net/1200x1800-new/20069/SM987381.jpg"
+            alt=""
+          />
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
