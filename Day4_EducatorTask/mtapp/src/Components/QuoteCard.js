a800877f9d59ef15fa0b03c46fe0414036901b1d
Jaiten <Jaitensahu9@gmail.com>
2023-12-21 11:46:54 +0530

    added animations

--- Day4_EducatorTask/mtapp/src/Components/QuoteCard.js
+++ Day4_EducatorTask/mtapp/src/Components/QuoteCard.js
@@ -2,7 +2,7 @@ import './QuoteCard.css'
  export default (props)=>{
     return (
         
-    <div className="card">
+    <div className="card" data-aos="fade-up">
         <h1>{props.quote}</h1>
         <p>{props.author}</p>
     </div>
